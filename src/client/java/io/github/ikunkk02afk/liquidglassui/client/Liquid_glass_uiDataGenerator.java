6385feb0fa72fcd6b1a531a5444a65231d28fdcf
ikunkk02-afk <ikunkk02@gmail.com>
2026-08-01 16:20:24 +0800

    chore: import Fabric 1.21.1 template

---
+++ src/client/java/io/github/ikunkk02afk/liquidglassui/client/Liquid_glass_uiDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.ikunkk02afk.liquidglassui.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Liquid_glass_uiDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
